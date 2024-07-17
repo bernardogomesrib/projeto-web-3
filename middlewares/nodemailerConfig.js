cf5105fb38c726817e891e1746f65b737aec1c3f
Thiago Lins <thiago.jorge2001@gmail.com>
2024-07-17 01:19:56 -0300

    Instalando e configurando Nodemailer

---
+++ middlewares/nodemailerConfig.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const nodemailer = require('nodemailer');
+
+const smtpTransporter = nodemailer.createTransport({
+  service: process.env.EMAIL_SERVICE,
+  host: process.env.EMAIL_HOST,
+  port: process.env.EMAIL_PORT,
+  secure: true,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  }
+});
+
+module.exports = smtpTransporter;
