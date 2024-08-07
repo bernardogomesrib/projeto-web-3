8fd9e6f86836c651bf98300d59a7ecd83c27345f
ThiagoDaHora <55456331+thiagojorgelins@users.noreply.github.com>
2024-08-07 11:47:53 -0300

    Adicionando upload de imagens no Firebase

--- controllers/ThreadControl.js
+++ controllers/ThreadControl.js
@@ -4,17 +4,18 @@ const Thread = require("../entities/Thread");
 const Board = require("../entities/Board");
 
 const ThreadControl = {
-
     async getAll(req, res) {
         // #swagger.tags = ['Thread']
-        const { page, size } = req.query
+        const { page, size } = req.query;
         const pageNumber = Math.max(1, Number(page) || 1);
-        const sizeNumber = 20
+        const sizeNumber = 20;
         try {
-            res.json(await Thread.findAll({
-                limit: sizeNumber,
-                offset: (pageNumber - 1) * sizeNumber,
-            }));
+            res.json(
+                await Thread.findAll({
+                    limit: sizeNumber,
+                    offset: (pageNumber - 1) * sizeNumber,
+                })
+            );
         } catch (error) {
             res.status(500).json({
                 error: "Erro ao buscar os Threads - " + error.message,
@@ -27,21 +28,21 @@ const ThreadControl = {
     async searchThreads(req, res) {
         // #swagger.tags = ['Thread']
         try {
-            const { filters } = req.params
+            const { filters } = req.params;
             const threads = await Thread.findAll({
                 where: {
                     [Op.or]: [
                         {
                             titulo: {
-                                [Op.like]: `%${filters}%`
-                            }
+                                [Op.like]: `%${filters}%`,
+                            },
                         },
                         {
                             mensagem: {
-                                [Op.like]: `%${filters}%`
-                            }
-                        }
-                    ]
+                                [Op.like]: `%${filters}%`,
+                            },
+                        },
+                    ],
                 },
             });
             res.json(threads);
@@ -94,38 +95,30 @@ const ThreadControl = {
     },
 
     async save(req, res) {
-        // #swagger.tags = ['Thread']
+        //  #swagger.tags = ['Thread']
+
         const board = req.params.board;
         const user = req.user;
-        const { titulo, mensagem, arquivo } = req.body;
+        const { titulo, mensagem } = req.body;
+        const arquivo = req.file ? req.file.firebaseUrl : null;
         const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
         try {
-            const boardId = await Board.findByPk(board)
+            const boardId = await Board.findByPk(board);
             if (!boardId) {
-                return res.status(404).json({
-                    error: "Board não encontrado",
-                });
+                return res.status(404).json({ error: "Board não encontrado" });
             }
-            if (!user) {
-                const ThreadInstance = await Thread.create({
-                    titulo,
-                    mensagem,
-                    arquivo,
-                    ip,
-                    boardId: board,
-                });
-                res.json(ThreadInstance);
-            } else {
-                const ThreadInstance = await Thread.create({
-                    titulo,
-                    mensagem,
-                    arquivo,
-                    ip,
-                    boardId: board,
-                    userId: user.id,
-                });
-                res.json(ThreadInstance);
+            const threadData = {
+                titulo,
+                mensagem,
+                arquivo,
+                ip,
+                boardId: board,
+            };
+            if (user) {
+                threadData.userId = user.id;
             }
+            const ThreadInstance = await Thread.create(threadData);
+            res.json(ThreadInstance);
         } catch (error) {
             res.status(500).json({
                 error: "Erro ao salvar Thread - " + error.message,
@@ -139,20 +132,19 @@ const ThreadControl = {
         try {
             return await Thread.findOne({
                 where: {
-                    title
-                }
-            })
+                    titulo: title,
+                },
+            });
         } catch (error) {
-            throw new Error('Erro ao buscar thread pelo titulo ' + error.message)
+            throw new Error("Erro ao buscar thread pelo titulo " + error.message);
         }
     },
 
     async updateThread(req, res) {
         // #swagger.tags = ['Thread']
-        // #swagger.security = [{ "Bearer": [] }]
-        // #swagger.parameters['id'] = { description: 'ID da thread' }
         const { id } = req.params;
-        const { titulo, mensagem, arquivo } = req.body;
+        const { titulo, mensagem } = req.body;
+        const arquivo = req.file ? req.file.firebaseUrl : null;
         const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
         const userLogged = req.user;
 
@@ -166,33 +158,33 @@ const ThreadControl = {
                 return res.status(401).json({ msg: "Unauthorized" });
             }
 
-            const updatedData = {}
+            const updatedData = {};
             if (titulo) {
-                const titleExist = await ThreadControl.getByTitle(titulo)
-                if(!titleExist){
-                    updatedData.titulo = titulo
+                const titleExist = await ThreadControl.getByTitle(titulo);
+                if (!titleExist) {
+                    updatedData.titulo = titulo;
                 } else {
-                    return res.status(400).json({ msg: "Titulo da thread já existe!"})
+                    return res.status(400).json({ msg: "Titulo da thread já existe!" });
                 }
             }
 
             if (mensagem) {
-                updatedData.mensagem = mensagem
+                updatedData.mensagem = mensagem;
             }
 
             if (arquivo) {
-                updatedData.arquivo = arquivo
+                updatedData.arquivo = arquivo;
             }
 
             updatedData.ip = ip;
 
             await ThreadInstance.update(updatedData, {
                 where: {
-                    id
-                }
+                    id,
+                },
             });
 
-            res.sendStatus(204)
+            res.sendStatus(204);
         } catch (error) {
             res.status(500).json({
                 error: "Erro ao atualizar Thread - " + error.message,
@@ -209,15 +201,13 @@ const ThreadControl = {
         const ThreadInstance = await Thread.findByPk(id);
         try {
             if (!ThreadInstance) {
-                return req.status(404).json({
-                    error: "Não existe a Thread",
-                });
+                return res.status(404).json({ error: "Não existe a Thread" });
             }
             await ThreadInstance.destroy();
-            res.status(201).json({ message: "Thread deletado" });
+            res.status(201).json({ message: "Thread deletada" });
         } catch (error) {
             res.status(500).json({
-                error: "erro ao deletar - " + error.message,
+                error: "Erro ao deletar - " + error.message,
                 name: error.name,
                 stack: error.stack,
             });
--- readme.md
+++ readme.md
@@ -24,6 +24,20 @@ EMAIL_USER="seu email do gmail"
 EMAIL_PASS="Senha de app gerada, necessário ter verificação de duas estapas"
 ```
 
+> Configuração Firebase 
+```
+type=""
+project_id=""
+private_key_id=""
+private_key=""
+client_id=""
+auth_uri=""
+token_uri=""
+auth_provider_x509_cert_url=""
+client_x509_cert_url=""
+universe_domain=""
+```
+
 Criar um database com o nome passado na variável _DB_NAME_
 
 > _npm run dev_
