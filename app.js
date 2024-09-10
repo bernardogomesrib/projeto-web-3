6736e1a48fc67ff5b1c9f0b15e85ef9eb658a901
ThiagoDaHora <tjlh@discente.ifpe.edu.br>
2024-09-10 09:43:11 -0300

    Add arquivos estaticos e imagens das boards

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const swaggerDocument = require("./swagger.json");
 const router = require("./routes/Routes");
 const cors = require("cors");
 const app = express();
+const path = require('path');
 
 app.use(
   cors({
@@ -16,6 +17,7 @@ app.use(
 );
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(router);
 
